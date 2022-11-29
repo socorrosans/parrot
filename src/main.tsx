073c524f2cbecbd640c511ef812e95aeb6101b1a
Socorro Santos <socorro.sans@gmail.com>
2022-11-29 17:32:52 -0300

    routes created

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import App from './App';
 import ReactDOM from 'react-dom/client';
+import {RouterProvider} from "react-router-dom";
+import {router} from "./routes";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import {createBrowserRouter} from "react-router-dom";
+import Feed from "../pages/Feed/Feed";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>
+  },
+  {
+    path: "/cadastrar",
+    element: <Register/>
+  },
+  {
+    path:"/feed",
+    element: <Feed/>
+  }
+])
